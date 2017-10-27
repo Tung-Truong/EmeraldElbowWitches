9abd05cf4577e33d6045d3b1148cd5aae9756f1e
joshuagalang <jagalang@wpi.edu>
2017-10-27 17:43:27 -0400

    Indenting is important.

--- teamlist.java
+++ teamlist.java
@@ -3,7 +3,7 @@ public class teamlist {
     public static void main(String[] args) {
         System.out.print("Karsten Roberts");
         System.out.print("Joshua Galang");
-	System.out.print("Clayton Dembski");
+	    System.out.print("Clayton Dembski");
     }
 
 }
