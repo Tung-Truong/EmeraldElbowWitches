315bd1645ff5033ab0da2f4246bd480046d4cb0f
Clayton Dembski <cdembski1@gmail.com>
2017-10-27 17:38:01 -0400

    clayton name

--- teamlist.java
+++ teamlist.java
@@ -1,8 +1,9 @@
 public class teamlist {
 
     public static void main(String[] args) {
-        System.out.print("Karsten Roberts")
-        System.out.print("Joshua Galang")
+        System.out.print("Karsten Roberts");
+        System.out.print("Joshua Galang");
+	System.out.print("Clayton Dembski");
     }
 
 }
