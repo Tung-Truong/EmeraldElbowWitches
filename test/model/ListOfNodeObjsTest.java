aea5b01d586d1c27d2a094ec38af6116c46efa79
Thomas Wiles <tgwiles@wpi.edu>
2017-11-11 21:28:36 -0500

    Added (successful) tests to EdgeObjTest

---
+++ test/model/ListOfNodeObjsTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListOfNodeObjsTest {
+    @Test
+    public void getDistance() throws Exception {
+    }
+
+    @Test
+    public void setEdgeWeight() throws Exception {
+    }
+
+    @Test
+    public void setAllNodeObjToGoal() throws Exception {
+    }
+
+    @Test
+    public void addNodeObjToList() throws Exception {
+    }
+
+    @Test
+    public void removeNodeObj() throws Exception {
+    }
+
+}
