430c72faa41385984cbd3b977fbdfff03c45ecc5
Krystal Grant <kgrant@wpi.edu>
2017-11-12 21:32:10 -0500

    Create test file for JanitorService class

---
+++ test/model/JanitorServiceTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JanitorServiceTest {
+    @Test
+    public void sendEmailServiceRequest() throws Exception {
+    }
+
+}
