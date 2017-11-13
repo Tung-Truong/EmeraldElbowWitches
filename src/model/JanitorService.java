b12b5b0b6f7d5bf420c1bfb386f3f6024f0f26a8
Krystal Grant <kgrant@wpi.edu>
2017-11-12 20:55:52 -0500

    Implement email service with necessary imports

--- src/model/JanitorService.java
+++ src/model/JanitorService.java
@@ -1,13 +1,47 @@
 package model;
 
 import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class JanitorService {
-    public JanitorService(){
+    private Properties properties;
+    private Session session;
+    private String username;
+    private String password;
 
+    public JanitorService(){
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+    }
+
+    public void setAccount(String username, String password){
+        this.username = username;
+        this.password = password;
     }
 
     public void sendEmailServiceRequest(String message){
+        try {
+            MimeMessage email = new MimeMessage(session);
 
+            email.setFrom(new InternetAddress("random@gmail.com"));
+            email.addRecipient(Message.RecipientType.TO, new InternetAddress("kgrant@wpi.edu"));
+            email.setSubject("Testing Testing 123");
+            email.setText(message);
+
+            Transport.send(email);
+        }
+        catch (MessagingException mex){
+            mex.printStackTrace();
+        }
     }
 }
