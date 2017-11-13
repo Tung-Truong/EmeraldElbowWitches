71a6c3716756887589a203322c740f2455e6210c
Krystal Grant <kgrant@wpi.edu>
2017-11-12 21:45:57 -0500

    Create test, test fails

--- test/model/JanitorServiceTest.java
+++ test/model/JanitorServiceTest.java
@@ -5,8 +5,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JanitorServiceTest {
+    JanitorService dave = new JanitorService();
+
     @Test
     public void sendEmailServiceRequest() throws Exception {
+        try {
+            dave.sendEmailServiceRequest("I'm so tired");
+        }
+        catch(Exception e){
+
+        }
     }
 
 }
