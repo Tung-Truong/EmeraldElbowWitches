45ca6266a1f55ed76c6c469f9111e15e187316b2
karstenroberts <karstenroberts@gmail.com>
2017-11-14 01:55:34 -0500

    Setup basic framework for map editing

--- src/controller/UI_v1.java
+++ src/controller/UI_v1.java
@@ -1,11 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -13,61 +13,161 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import model.*;
 import sun.plugin.javascript.navig4.Layer;
+
 import java.util.ArrayList;
 
 public class UI_v1 {
 
+    private enum CurrentStatus {
+        PATIENT, ADMIN, ADDNODE, REMOVENODE, MODIFYBORDERS
+    };
+
+    private CurrentStatus currentState = CurrentStatus.PATIENT;
+
     private GraphicsContext gc1;
 
-    @FXML
-    private MenuItem Map1;
 
     @FXML
-    private Canvas gc;
+    private Layer lineLayer;
 
     @FXML
-    private ImageView currentMap;
-
-    @FXML
-    private MenuButton MapDropDown;
-
-    @FXML
-    private MenuItem Map2;
+    private SplitPane AddLine;
 
     @FXML
     private AnchorPane mapContainer;
 
     @FXML
-    private Layer lineLayer;
+    private ImageView currentMap;
 
-    private static class Mpoint {
-        double x, y;
-    }
+    double mapWidth;
+    double mapHeight;
 
     @FXML
-    //This is where pathfinding is currently
-    //this shall be renamed later to something better
-    void addLine(MouseEvent event) throws InvalidNodeException {
-        //getX/stageWidth = w/5000
-        //5000*getX/stageWidth
+    private Canvas gc;
 
+    @FXML
+    private MenuButton MapDropDown;
+
+    @FXML
+    private MenuItem Map1;
+
+    @FXML
+    private MenuItem Map2;
+
+    @FXML
+    private Tab patientTab;
+
+    @FXML
+    private Tab adminTab;
+
+    @FXML
+    private AnchorPane requestconfirm;
+
+    @FXML
+    private Button addNodeSelector;
+
+    @FXML
+    private Button removeNodeSelector;
+
+    @FXML
+    private Button modifyBordersSelector;
+
+    @FXML
+    private TextField edgeWeight;
+
+    @FXML
+    private TextField edgeNodeA;
+
+    @FXML
+    private TextField edgeNodeB;
+
+    @FXML
+    private TextField Xloc;
+
+    @FXML
+    private TextField Yloc;
+
+    @FXML
+    private TextField Floor;
+
+    @FXML
+    private TextField Building;
+
+    @FXML
+    private TextField NodeType;
+
+    @FXML
+    private TextField ShortName;
+
+    @FXML
+    private TextField LongName;
+
+    @FXML
+    private TextField Team;
+
+    @FXML
+    private TextField NodeId;
+
+    @FXML
+    private Button addNodeConfirm;
+
+    @FXML
+    private TextField removeNode;
+
+    @FXML
+    private Button removeNodeConfirm;
+
+    @FXML
+    private Button edgeUpdate;
+
+    @FXML
+    private Button edgeDelete;
+
+    @FXML
+    private TextArea MessageTestBox;
+
+
+    @FXML
+        //This is where pathfinding is currently
+        //this shall be renamed later to something better
+    void stateHandler(MouseEvent event) {
         //get the mouses location and convert that to the corrected map coordinates for the original image
         Scene currScene = model.Main.getCurrScene();
-        double mapWidth = currentMap.getFitWidth();
-        double mapHeight = currentMap.getFitHeight();
-       // System.out.println(mapWidth + " " + mapHeight);
+        mapWidth = currentMap.getFitWidth();
+        mapHeight = currentMap.getFitHeight();
+        // System.out.println(mapWidth + " " + mapHeight);
 
-        double mousex = (5000*event.getX())/mapWidth;
-        double mousey = (3400*event.getY())/mapHeight;
+        double mousex = (5000 * event.getX()) / mapWidth;
+        double mousey = (3400 * event.getY()) / mapHeight;
+
+        switch (currentState){
+            case PATIENT:
+                //pathfind
+                break;
+            case ADMIN:
+                break;
+            case ADDNODE:
+                //edit/add node
+                break;
+            case REMOVENODE:
+                //delete node, attached edges
+                break;
+            case MODIFYBORDERS:
+                //edit/delete/add edge
+                break;
+        }
+
         //Print to confirm
 
         //convert click resolution to map ratio
-        System.out.println((5000*event.getX())/mapWidth + " " + (3400*event.getY())/mapHeight);
+        System.out.println((5000 * event.getX()) / mapWidth + " " + (3400 * event.getY()) / mapHeight);
         //far left stair node
-        System.out.println("Far left stair coords: " + 2190 + " " + 910);
-        //Rehab center
-        System.out.println("Rehab center coords: " + 2790 + " " + 1380);
 
+
+
+    }
+
+    public void findPath(int mousex, int mousey) throws InvalidNodeException{
         //create a new astar object
         astar newpathGen = new astar();
         this.gc1 = gc.getGraphicsContext2D();
@@ -76,33 +176,28 @@ public class UI_v1 {
         gc1.setFill(Color.RED);
 
         //get node that corr. to click from ListOfNodeObjects made in main
-        NodeObj goal = model.Main.getNodeMap().getNearestNeighborFilter
-                ((int)Math.floor(mousex), (int)Math.floor(mousey));
-
-       /*NodeObj goal = model.Main.getNodeMap().getNearestNeighborFilter
-                (1636, 1933);*/
-
-       // NodeObj goal = model.Main.getNodeMap().getNearestNeighborFilter
-              //  (2790, 1380);
-//2788 1389
+        NodeObj goal = null;
+        try {
+            goal = Main.getNodeMap().getNearestNeighborFilter
+                    ((int) Math.floor(mousex), (int) Math.floor(mousey));
+        } catch (InvalidNodeException e) {
+            e.printStackTrace();
+        }
         //getStart
         NodeObj Kiosk = model.Main.getKiosk();
         //set the path to null
         ArrayList<NodeObj> path = null;
-        /*gc1.fillOval(goal.node.getxLoc()*mapWidth/5000-5,
-                goal.node.getyLoc()*mapHeight/3400-5,
-                10,
-                10);*/
-        gc1.fillOval(Kiosk.node.getxLoc()*mapWidth/5000-5,
-                Kiosk.node.getyLoc()*mapHeight/3400-5,
-                10,
-                10);
+
         //try a*
-        /*if(newpathGen.pathfind(Kiosk,goal,gc1))
+        if(newpathGen.pathfind(Kiosk,goal,gc1))
             path = newpathGen.getGenPath();
         else
-            throw new InvalidNodeException("this is not accessable with the current map");
-        */
+            try {
+                throw new InvalidNodeException("this is not accessable with the current map");
+            } catch (InvalidNodeException e) {
+                e.printStackTrace();
+            }
+
         gc1.setGlobalAlpha(.5);
         gc1.setStroke(Color.BLUE);
         gc1.setFill(Color.BLUE);
@@ -122,28 +217,40 @@ public class UI_v1 {
             }
 
         }*/
+    }
 
-
-
-        gc1.setFill(Color.RED);
-        System.out.println(mapContainer.getChildren());
-
-        gc1.fillText(goal.node.getNodeID(),goal.node.getxLoc()*mapWidth/5000-5,
-                goal.node.getyLoc()*mapHeight/3400-5);
-        gc1.fillOval(Kiosk.node.getxLoc()*mapWidth/5000-5,
-                Kiosk.node.getyLoc()*mapHeight/3400-5,
-                10,
-                10);
-
+    @FXML
+    void addNodeFlag() {
 
     }
 
     @FXML
+    void modifyBordersFlag() {
+
+    }
+
+    @FXML
+    void removeNodeFlag() {
+
+    }
+
+    @FXML
+    void switchTab() {
+
+    }
+
+
+    private static class Mpoint {
+        double x, y;
+    }
+
+
+    @FXML
     void GetMap1() {
         setKioskLoc(2460, 910);
-        gc1.clearRect(0,0,currentMap.getFitWidth(), currentMap.getFitHeight());
+        gc1.clearRect(0, 0, currentMap.getFitWidth(), currentMap.getFitHeight());
         MapDropDown.setText("45 Francis Floor 1 Center");
-        Image m1 =  new Image("file:src/view/media/basicMap.png");
+        Image m1 = new Image("file:src/view/media/basicMap.png");
         currentMap.setImage(m1);
         Main.getNodeMap().setCurrentBuilding("45 Francis");
         //need to set currentMap of ListOfNodeObjs to "
@@ -152,18 +259,17 @@ public class UI_v1 {
     @FXML
     void GetMap2() {
         setKioskLoc(1580, 1810);
-        gc1.clearRect(0,0,currentMap.getFitWidth(), currentMap.getFitHeight());
+        gc1.clearRect(0, 0, currentMap.getFitWidth(), currentMap.getFitHeight());
         MapDropDown.setText("Shapiro Building Floor 2");
-        Image m2 =  new Image("file:src/view/media/Shapiro.png");
+        Image m2 = new Image("file:src/view/media/Shapiro.png");
         currentMap.setImage(m2);
         Main.getNodeMap().setCurrentBuilding("Shapiro");
     }
 
-    void setKioskLoc(int xCoord, int yCoord){
+    void setKioskLoc(int xCoord, int yCoord) {
         try {
             Main.setKiosk(Main.getNodeMap().getNearestNeighbor(xCoord, yCoord));
-        }
-        catch (InvalidNodeException e){
+        } catch (InvalidNodeException e) {
             e.printStackTrace();
         }
     }
