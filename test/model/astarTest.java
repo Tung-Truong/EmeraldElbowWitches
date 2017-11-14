555890baac377888029ee894ab1098eb1dfab299
Thomas Wiles <tgwiles@wpi.edu>
2017-11-14 16:35:31 -0500

    added astar test class and changed minor error in map selection button

---
+++ test/model/astarTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class astarTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void pathfind() throws Exception {
+    }
+
+    @Test
+    public void constructPath() throws Exception {
+    }
+
+    @Test
+    public void addToQueue() throws Exception {
+    }
+
+    @Test
+    public void getGenPath() throws Exception {
+    }
+
+}
