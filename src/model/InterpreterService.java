1b31983a5f7cfb8898efdea683741a5caa276ced
Krystal Grant <kgrant@wpi.edu>
2017-11-20 16:16:01 -0500

    Flesh out InterpreterService class

--- src/model/InterpreterService.java
+++ src/model/InterpreterService.java
@@ -1,4 +1,31 @@
 package model;
 
 public class InterpreterService extends ServiceRequest {
+
+    // Attributes
+    private String[] languages;
+    private String[] emails;
+
+    // Constructors
+    public InterpreterService(){
+
+    }
+
+    // Getters
+    public String[] getLanguages() {
+        return this.languages;
+    }
+
+    public String[] getEmails() {
+        return this.emails;
+    }
+
+    // Setters
+    public void setLanguages(String[] languages){
+        this.languages = languages;
+    }
+
+    public void setEmails(String[] emails){
+        this.emails = emails;
+    }
 }
