ced6f03c6074752d4632a5689b6b11626f87ffba
Krystal Grant <kgrant@wpi.edu>
2017-11-20 16:11:32 -0500

    Flesh out CafeteriaService class

--- src/model/CafeteriaService.java
+++ src/model/CafeteriaService.java
@@ -2,8 +2,30 @@ package model;
 
 public class CafeteriaService extends ServiceRequest {
 
-    private String[] menu;
+    // Attributes
+    private String[] menu = {"Cake", "French Fries", "Soup",
+                            "Water", "Club Sandwich"};
     private String[] emails;
 
+    // Constructor
+    public CafeteriaService() {
+        // TODO: get emails from database and set them here
+    }
+    // Getters
 
+    public String[] getMenu() {
+        return this.menu;
+    }
+
+    public String[] getEmails() {
+        return this.emails;
+    }
+
+    // Setters
+    public void setMenu(String[] items){
+        this.menu = items;
+    }
+    public void setEmails(String[] emails){
+        this.emails = emails;
+    }
 }
