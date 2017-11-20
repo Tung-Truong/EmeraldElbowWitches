3c38bfbbe85ec21c47e79ee6d7e4b8d93d323055
Krystal Grant <kgrant@wpi.edu>
2017-11-20 15:34:03 -0500

    Add basic attributes, constructor, and comments for ServiceController

--- src/controller/ServiceController.java
+++ src/controller/ServiceController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import model.ServiceRequest;
+
 public class ServiceController {
+    // Attributes
+    private String serviceNeeded;
+    private ServiceRequest service;
+
+    // Constructor
+    public ServiceController (){
+
+    }
+
+    // Getters
+
+    // Setters
 }
