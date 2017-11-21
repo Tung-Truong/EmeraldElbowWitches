f2a62872c14ff79bcc849f68945adaa946bc6609
Clayton Dembski <cdembski1@gmail.com>
2017-11-21 13:53:11 -0500

    added fxml to service requests

--- src/controller/ServiceController.java
+++ src/controller/ServiceController.java
@@ -4,14 +4,54 @@ import model.CafeteriaService;
 import model.InterpreterService;
 import model.JanitorService;
 import model.ServiceRequest;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 public class ServiceController {
+
+    @FXML
+    private MenuButton EmployeeDropdown;
+
+    @FXML
+    private Button removeNodeSelector;
+
+    @FXML
+    private MenuButton FoodDropdown;
+
+    @FXML
+    private MenuButton LocationDropdown;
+
+    @FXML
+    private MenuButton RequestServiceDropdown;
+
+    @FXML
+    private JFXTextField NotesTextFeild;
+
+    @FXML
+    private MenuItem AdmGrant;
+
+
+
+    @FXML
+    void BackToUIV1( ) {
+
+    }
+
+    @FXML
+    void SubmitRequest( ) {
+
+    }
+
+
     // Attributes
     private String serviceNeeded;
     private ServiceRequest service;
 
     // Constructor
-    public ServiceController (String needed){
+    public void Service (String needed){
         this.serviceNeeded = needed;
 
         if(needed.toUpperCase() == "INTERPRETER"){
@@ -38,6 +78,54 @@ public class ServiceController {
     }
 
     public void setService(ServiceRequest serve){
+
+    }
+
+    @FXML
+    void MateninceItem() {
+        RequestServiceDropdown.setText("Maintenance");
+    }
+
+    @FXML
+    void InterpreterItem( ) {
+        RequestServiceDropdown.setText("Interpreter");
+    }
+
+    @FXML
+    void NoodlesItem( ) {
+        FoodDropdown.setText("Noodles");
+    }
+
+    @FXML
+    void BurgerItem( ) {
+        FoodDropdown.setText("Burger");
+    }
+
+    @FXML
+    void TapiocaItem( ) {
+        FoodDropdown.setText("Tapioca");
+    }
+
+
+    @FXML
+    void WaitingRoomItem( ) {
+        LocationDropdown.setText("WaitingRoom");
+    }
+
+    @FXML
+    void XrayItem( ) {
+        LocationDropdown.setText("Xray");
+
+    }
+
+    @FXML
+    void DrDembskiItem( ) {
+        EmployeeDropdown.setText("Dr. Dembski");
+    }
+
+    @FXML
+    void AdmGrantItem( ) {
+        EmployeeDropdown.setText("Admin Grant");
 
     }
 }
