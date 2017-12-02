e0a44e4f8bca9af5acaad97f6dc546e8998b3d41
karstenroberts <karstenroberts@gmail.com>
2017-12-02 15:17:35 -0500

    Add set start node feature

--- src/controller/PatientController.java
+++ src/controller/PatientController.java
@@ -7,6 +7,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import model.ImageLoader;
 import model.InvalidNodeException;
 import model.astar;
@@ -23,6 +24,7 @@ import javafx.scene.paint.Color;
 import model.*;
 
 import java.util.ArrayList;
+import java.util.Timer;
 
 
 public class PatientController extends Controller {
@@ -48,68 +50,25 @@ public class PatientController extends Controller {
     @FXML
     private JFXButton btn_mapL2;
 
-    private GraphicsContext gc1 = null;
-
-    public static TextDirections textDirections = new TextDirections();
-
-    private int XTrans = 0;
-
-    private int YTrans = 0;
-
-    private double Zoom = 1;
-
-    ArrayList<NodeObj> currPath = null;
-
-    NodeObj goal = null;
-
-    private PathingContainer currentAlgorithm = new PathingContainer();
-
-    @FXML
-    private TextArea directionsBox;
-
-    @FXML
-    private TextField startNodeID;
-
-    @FXML
-    private TextField EndNodeID;
-
-    @FXML
-    private AnchorPane mapContainer;
-
-    @FXML
-    public ImageView currentMap;
-
-    double mapWidth;
-    double mapHeight;
-
     @FXML
     private Canvas gc;
 
     @FXML
-    private MenuButton MapDropDown;
-
-    @FXML
-    private MenuItem Map1;
-
-    @FXML
-    private MenuItem Map2;
-
-    @FXML
-    private MenuItem MapL2;
-
-    @FXML
-    private MenuItem MapL1;
-
-    @FXML
-    private MenuItem MapG;
-
-    @FXML
-    private MenuItem Map3;
-
-    @FXML
     private ImageView homeScreen;
 
+    @FXML
+    public ImageView currentMap;
 
+    private GraphicsContext gc1 = null;
+    public static TextDirections textDirections = new TextDirections();
+    private int XTrans = 0;
+    private int YTrans = 0;
+    private double Zoom = 1;
+    ArrayList<NodeObj> currPath = null;
+    NodeObj goal = null;
+    private PathingContainer currentAlgorithm = new PathingContainer();
+    double mapWidth;
+    double mapHeight;
     ImageLoader mapImage = new ImageLoader();
 
     public void initialize() {
@@ -119,6 +78,10 @@ public class PatientController extends Controller {
         mapWidth = currentMap.getFitWidth();
         mapHeight = currentMap.getFitHeight();
         setKioskLoc(2460, 910);
+        redraw();
+    }
+
+    private void redraw(){
         if (gc1 == null)
             gc1 = gc.getGraphicsContext2D();
         gc1.clearRect(0, 0, currentMap.getFitWidth(), currentMap.getFitHeight());
@@ -206,7 +169,7 @@ public class PatientController extends Controller {
     /*
      * findPath pathfinds, and draws the route to the screen
      */
-    public void findPath(MouseEvent event) throws InvalidNodeException{
+    public void findPath(MouseEvent event){
         //create a new astar object
 
         double mousex = (5000 * event.getX()) / mapWidth;
@@ -246,4 +209,78 @@ public class PatientController extends Controller {
     }
 
 
+    public void findPath(NodeObj dest){
+        //create a new astar object
+        if(gc1 == null)
+            gc1 = gc.getGraphicsContext2D();
+        gc1.clearRect(0, 0, currentMap.getFitWidth(), currentMap.getFitHeight());
+        gc1.setLineWidth(2);
+        gc1.setStroke(Color.BLUE);
+        gc1.setFill(Color.RED);
+        //get node that corr. to click from ListOfNodeObjects made in main
+        goal = dest;
+        //getStart
+        NodeObj Kiosk = Main.getKiosk();
+        //set the path to null
+        ArrayList<NodeObj> path;
+        if(!Kiosk.getNode().getNodeID().equals(goal.getNode().getNodeID())) {
+            //try a*
+            if (currentAlgorithm.getPathAlg().pathfind(Kiosk, goal)) {
+                path = currentAlgorithm.getPathAlg().getGenPath();
+                currPath = path;
+                //directionsBox.setText(textDirections.getTextDirections(path));
+            } else {
+                try {
+                    throw new InvalidNodeException("this is not accessable with the current map");
+                } catch (InvalidNodeException e) {
+                    e.printStackTrace();
+                }
+            }
+            DrawCurrentFloorPath();
+        }
+    }
+
+
+    @FXML
+    void mousePress(MouseEvent event){
+        if(event.getButton() == MouseButton.PRIMARY){
+            findPath(event);
+        }else if((event.getButton() == MouseButton.SECONDARY) || ((event.getButton() == MouseButton.PRIMARY) && (event.isControlDown()))){
+            setStartNode(event);
+        }
+    }
+
+
+    /*
+     * setStartNode sets the start node to the node nearest to the given coords in the current building
+     */
+    @FXML
+    void setStartNode(MouseEvent event){
+        double mousex = (5000 * event.getX()) / mapWidth;
+        double mousey = (3400 * event.getY()) / mapHeight;
+        String newStartNodeID = null;
+        try {
+            newStartNodeID = Main.getNodeMap().getNearestNeighborFilter((int)mousex,(int)mousey).getNode().getNodeID();
+        } catch (InvalidNodeException e) {
+            e.printStackTrace();
+        }
+        NodeObj newStartNode = Main.getNodeMap().getNodeObjByID(newStartNodeID);
+        Main.setKiosk(newStartNode);
+        redraw();
+    }
+
+/*    @FXML
+    void setEndNode(MouseEvent event){
+        double mousex = (5000 * event.getX()) / mapWidth;
+        double mousey = (3400 * event.getY()) / mapHeight;
+        String newEndNodeID = EndNodeID.getText();
+        NodeObj newEndNode = Main.getNodeMap().getNodeObjByID(newEndNodeID);
+        redraw();
+        try {
+            findPath(newEndNode);
+        } catch (InvalidNodeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Did you get here");
+    }*/
 }
