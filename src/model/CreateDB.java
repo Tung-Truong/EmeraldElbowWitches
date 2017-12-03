174d94ef84b1613365589c672c0f8cc2fdee5d21
Krystal Grant <kgrant@wpi.edu>
2017-12-02 19:18:37 -0500

    Application will now launch, currently filling ServiceRequest.csv with some random dummy data

--- src/model/CreateDB.java
+++ src/model/CreateDB.java
@@ -14,7 +14,7 @@ public class CreateDB {
         connection.createStatement().execute("create table nodeTable(nodeID VARCHAR(255), xLoc VARCHAR(255), yLoc VARCHAR(255), floor VARCHAR(255), building VARCHAR(255), nodeType VARCHAR(255), shortName VARCHAR(255), longName VARCHAR(255), team VARCHAR(255))");
         connection.createStatement().execute("create table edgeTable(edgeID VARCHAR(255), startNode VARCHAR(255), endNode VARCHAR(255))");
         connection.createStatement().execute("create table employeeTable(email VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255), department VARCHAR(255), language VARCHAR(255), availability VARCHAR(255))");
-        connection.createStatement().execute("create table requestTable(employeeId, VARCHAR(255), requestType VARCHAR(255), timeSubmitted VARCHAR(255), isActive VARCHAR(255))");
+        connection.createStatement().execute("create table requestTable(employeeId VARCHAR(255), requestType VARCHAR(255), timeSubmitted VARCHAR(255), isActive VARCHAR(255))");
         System.out.print("The table has been built!");
     }
 }
